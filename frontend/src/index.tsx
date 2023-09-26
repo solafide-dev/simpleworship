09b8a3071e0cbbceee003d98537282c0b8ef28a5
Cameron Chunn <cameronchunn@gmail.com>
2023-09-26 16:19:24 -0500

    log the mutation events in the console

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,11 +8,22 @@ import Display from './Display';
 import './offscreenCanvasPolyfill.js'
 
 import { EventsOn } from '../wailsjs/runtime';
+import { GetOrderOfService, GetSong } from '../wailsjs/go/main/App';
 
 (() => {
   // This is probably not remotely ok in react but I'm not a frontend dev so ¯\_(ツ)_/¯
   EventsOn("data-mutate", (data: any) => {
     console.log("[DATA MUTATED]",data)
+    if (data.dataType === "Song") {
+      GetSong(data.id).then((song) => {
+        console.log("[SONG]",song)
+      })
+    }
+    if (data.dataType === "OrderOfService") {
+      GetOrderOfService(data.id).then((oos) => {
+        console.log("[OOS]",oos)
+      })
+    }
   })
 })();
 
